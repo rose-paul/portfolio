917436b9c752ca3ba5eeff2f2e74dbcbbf0ccb8b
Paul <prose0021@gmail.com>
2019-12-16 14:58:49 -0800

    Updates

---
+++ precache-manifest.5229e7427a7c54fccf4a43717febff25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804b67f6d101510775c55e8ca43de420",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ddaf35efbd58efa365eb",
+    "url": "/portfolio/static/css/main.f441dddc.chunk.css"
+  },
+  {
+    "revision": "4ae07f9c0a16468aec51",
+    "url": "/portfolio/static/js/2.1dff7fbb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.1dff7fbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ddaf35efbd58efa365eb",
+    "url": "/portfolio/static/js/main.51617961.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  }
+]);
