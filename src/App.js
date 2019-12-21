304f3585615ec2c811b1b7955fcdb1b75a086ae6
Paul <prose0021@gmail.com>
2019-12-21 13:33:51 -0800

    update info

--- src/App.js
+++ src/App.js
@@ -24,19 +24,20 @@ function App() {
           <h2>About Me</h2>
           <article>
             I came into the tech realm as a developer following my previous work 
-            teaching English at tech companies in Moscow. My current project is developing a game Moscow Cows 
-            using JavaScript and HTML5 canvas. Recently, I was part of a team that developed 
-            an app called Ride, which allows motorcycle riders to map out rides and share them. 
-            For that project we used MongoDB, Express.js, React, Node.js. Upon completing my current projects, 
-            I am going expand my knowledge of React and begin learning C++.
+            teaching English at tech companies in Moscow. I do fullstack web development using several technologies and enjoy all 
+            aspects. My core interest lies in frontend engineering using React, Redux and Javascript. 
+            My enthusiasm for this aspect is driven by 2 things: the practicality of mapping the backend to a usable frontend 
+            and the challenge of optimizing this connection. Javascript and React are great means to this end, 
+            and I am always expanding my knowledge about them. Below you can find my projects 
+            and a list of the main technologies and languages I employ.
         </article>
         <br/>
         <article>
-            Outside of programming, I love to travel and am a big hockey fan. 
-            While I've been on several great trips over the past few years, 
+            Outside of programming, I love to travel. From 2015-2019 I lived in Moscow, Russia. I was fortunate to explore a lot of places
+            during my time there. While I've been on several great trips over the past several years, 
             the Kamchatka peninsula stands out the most. Snowmobiling through the most 
             remote parts of Russia in -40 degrees isn't a situation I imagined ever being in.
-            The pictures above include some of that trip, among others...
+            The pictures above include some of that trip, among others.
           </article>
         </header>
       </section>
