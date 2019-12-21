54c714a54370ed11c65da12d43e8c42f54c2cce8
Paul <prose0021@gmail.com>
2019-12-21 13:41:26 -0800

    add info

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
         <br/>
         <article>
             Outside of programming, I love to travel. From 2015-2019 I lived in Moscow, Russia. I was fortunate to explore a lot of places
-            during my time there. While I've been on several great trips over the past several years, 
+            during my time there, both in the country and the surrounding region. While I've been on several great trips over the past several years, 
             the Kamchatka peninsula stands out the most. Snowmobiling through the most 
             remote parts of Russia in -40 degrees isn't a situation I imagined ever being in.
             The pictures above include some of that trip, among others.
