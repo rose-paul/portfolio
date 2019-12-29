2d77cf41430b33ae63ef6235251932509a1e1c40
Paul <prose0021@gmail.com>
2019-12-29 16:57:58 -0600

    refactor about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './header';
+import Header from './header.jsx';
+import About from './about';
+
 
 // import Banner from "./components/banner"
 function App() {
@@ -9,32 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <div className="content">
-        <section className="about">
-            <h2>About Me</h2>
-            <article>
-              I came into the tech realm as a developer following my previous
-              work teaching English at tech companies in Moscow. I do fullstack
-              web development using several technologies and enjoy all aspects.
-              My core interest lies in frontend engineering using React, Redux
-              and Javascript. My enthusiasm for this aspect is driven by 2
-              things: the practicality of mapping the backend to a usable
-              frontend and the challenge of optimizing this connection.
-              Javascript and React are great means to this end, and I am always
-              expanding my knowledge about them. Below you can find my projects
-              and a list of the main technologies and languages I employ.
-            </article>
-            <br />
-            <article>
-              Outside of programming, I love to travel. From 2015-2019 I lived
-              in Moscow, Russia. I was fortunate to explore a lot of places
-              during my time there, both in the country and the surrounding
-              region. While I've been on several great trips over the past
-              several years, the Kamchatka peninsula stands out the most.
-              Snowmobiling through the most remote parts of Russia in -40
-              degrees isn't a situation I imagined ever being in. The pictures
-              above include some of that trip, among others.
-            </article>
-        </section>
+       <About />
         <section className="projects">
           <h2>Projects</h2>
           <div>
