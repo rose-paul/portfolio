49cc28628c75e3970aee0d6f8f15edff64e691ac
Paul <prose0021@gmail.com>
2019-12-31 09:33:05 -0600

    add animation to projects

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,28 +1,51 @@
 import React from 'react';
 
 const Projects = () => {
+
+    const mouseEnterAnim = (e) => {
+        let stylee = "rotateY(35deg)";
+        e.target.style.transform = stylee;
+        e.target.style.webkitTransform = stylee;
+        e.target.style.mozTransform = stylee;
+        e.target.style.msTransform = stylee;
+        e.target.style.oTransform = stylee;
+    }
+
+    const mouseLeaveAnim = (e) => {
+        e.target.style = "";
+    }
+
     return (
-    <section className="projects">
+      <section className="projects">
         <h2>Projects</h2>
         <div>
-            <a
-                href="https://gramcrackers.herokuapp.com/#/login"
-                target="_blank"
-            >
-                <img src="gramcrackers.png" />
-                <p>Gramcrackers</p>
-            </a>
-            <a href="https://rose-paul.github.io/MoscowCows/" target="_blank">
-                <img src="matryoshka.png" />
-                <p>Moscow Cows</p>
-            </a>
-            <a href="http://ride-mern.herokuapp.com/#/" target="_blank">
-                <img src="wheel-128.png" />
-                <p>Ride</p>
-            </a>
+          <a href="https://gramcrackers.herokuapp.com/#/login" target="_blank">
+            <img
+              src="gramcrackers.png"
+              onMouseEnter={mouseEnterAnim}
+              onMouseLeave={mouseLeaveAnim}
+            />
+            <p>Gramcrackers</p>
+          </a>
+          <a href="https://rose-paul.github.io/MoscowCows/" target="_blank">
+            <img
+              src="matryoshka.png"
+              onMouseEnter={mouseEnterAnim}
+              onMouseLeave={mouseLeaveAnim}
+            />
+            <p>Moscow Cows</p>
+          </a>
+          <a href="http://ride-mern.herokuapp.com/#/" target="_blank">
+            <img
+              src="wheel-128.png"
+              onMouseEnter={mouseEnterAnim}
+              onMouseLeave={mouseLeaveAnim}
+            />
+            <p>Ride</p>
+          </a>
         </div>
-    </section>
-    )
+      </section>
+    );
 }
 
 export default Projects;
