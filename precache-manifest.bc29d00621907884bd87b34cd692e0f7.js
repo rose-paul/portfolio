ceda30a49eacf462d2ccc2b3d8b93aa2095c3a46
Paul <prose0021@gmail.com>
2019-12-31 10:04:50 -0600

    Updates

---
+++ precache-manifest.bc29d00621907884bd87b34cd692e0f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4efcda3e01a967b2117a946569003a4a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e67ad8f519716cbf1e88",
+    "url": "/portfolio/static/css/main.09e226d2.chunk.css"
+  },
+  {
+    "revision": "0b9897d4e4f3fed4159b",
+    "url": "/portfolio/static/js/2.8155fff4.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/portfolio/static/js/2.8155fff4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e67ad8f519716cbf1e88",
+    "url": "/portfolio/static/js/main.98aaa6e1.chunk.js"
+  },
+  {
+    "revision": "16c109c584451046bd84",
+    "url": "/portfolio/static/js/runtime-main.860ade9b.js"
+  }
+]);
