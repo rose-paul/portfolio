840e58a152b52a9194474e9529e94821a83e5119
Paul <prose0021@gmail.com>
2020-01-11 16:56:26 -0800

    fix email

--- src/header.jsx
+++ src/header.jsx
@@ -12,40 +12,42 @@ const Header = () => {
         )
     }, [])
 
-    const onClick = () => {
-        window.location.href = "mailto:prose0021@gmail.com?subject=Hey%20Paul!&body="
-    }
-
-  return (  
-
+  return (
     <CSSTransition
-              in={showHeader}
-              timeout={4000}
-              classNames="header-transition"
-              appear
-          >
-          <header className={cx("App-header", "header-transition--active")}>
+      in={showHeader}
+      timeout={4000}
+      classNames="header-transition"
+      appear
+    >
+      <header className={cx("App-header", "header-transition--active")}>
         <img alt="" src="pic.jpeg" />
         <div>
-            <h1>Paul Rose</h1>
-            <h3>Software Engineer </h3>
-            <h3>React, Redux, Node.js, Rails </h3>
-            <div className="header-links">
-                <a href="https://github.com/rose-paul" target="_blank" rel="noopener noreferrer">
-                    <img alt="" src="001-github.png" />
-                </a>
-                <a
-                    href="https://www.linkedin.com/in/paul-rose-72626952/"
-                    target="_blank" rel="noopener noreferrer"
-                >
-                    <img alt="" src="002-linkedin.png" />
-                </a>
-                <img alt="" src="003-email.png" onClick={onClick}></img>
-            </div>
+          <h1>Paul Rose</h1>
+          <h3>Software Engineer </h3>
+          <h3>React, Redux, Node.js, Rails </h3>
+          <div className="header-links">
+            <a
+              href="https://github.com/rose-paul"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt="" src="001-github.png" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/paul-rose-72626952/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img alt="" src="002-linkedin.png" />
+            </a>
+            <a href="mailto:prose0021@gmail.com">
+              <img alt="" src="003-email.png" />
+            </a>
+          </div>
         </div>
-        </header>
+      </header>
     </CSSTransition>
-    )}
+  );}
 
 
 export default Header;
