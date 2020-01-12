92400398ff5ba637886eee1fc40f7e5185faaf4e
Paul <prose0021@gmail.com>
2020-01-11 16:42:26 -0800

    change about

--- src/about.jsx
+++ src/about.jsx
@@ -9,11 +9,15 @@ const About = () => {
        />
        <section className="about">
          <h2>About Me</h2>
-         <p>Skills: React, Redux, Javascript, Ruby on Rails, Mapbox, SQL, PostgresSQL, MongoDB</p>
+         <p>
+           Skills: React, Redux, Javascript, Ruby on Rails, Mapbox, SQL,
+           PostgresSQL, MongoDB
+         </p>
          <article>
            I came into the tech realm as a developer following my previous work
-           teaching English at tech companies in Moscow. I do fullstack web
-           development using several technologies and enjoy all aspects.
+           teaching English at tech companies in Moscow. Currently, I'm seeking
+           work! I do fullstack web development using several technologies and
+           enjoy all aspects.
          </article>
          <article>
            My core interest lies in frontend engineering using React, Redux and
