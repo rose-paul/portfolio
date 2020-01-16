3fb3e75a6efc181dd10369aed9ffba6b730b0079
Paul <prose0021@gmail.com>
2020-01-15 16:55:00 -0800

    add animations

--- src/projects.jsx
+++ src/projects.jsx
@@ -29,6 +29,36 @@ const Projects = () => {
         <h2>Projects</h2>
         <div>
           <div id="single-project">
+            <a
+              href="http://ride-mern.herokuapp.com/#/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt=""
+                src="wheel-128.png"
+                onMouseEnter={rotateWheel}
+                onMouseLeave={mouseLeaveAnim}
+              />
+              <p>Ride</p>
+            </a>
+            <div>
+              <p>
+                Ride is a web application targeting the motorcycling enthusiast
+                community. It allows a user to record a ride on a map and peruse
+                all saved rides. Use the "post" a ride feature to share it or
+                stop by Ride and find your next adventure. Leave the community
+                your nugget of wisdom in a ride review. Our aim is to enhance
+                the enjoyment of two wheeled riding everywhere.
+                <ul>
+                  <li>Frontend: React / Redux, Mapbox</li>
+                  <li>Backend: Express.js</li>
+                  <li>DB: MongoDB</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+          <div id="single-project">
             <div>
               <a
                 href="https://gramcrackers.herokuapp.com/#/login"
@@ -79,36 +109,6 @@ const Projects = () => {
                 cows.
                 <ul>
                   <li>Frontend: JavaScript, HTML5 Canvas</li>
-                </ul>
-              </p>
-            </div>
-          </div>
-          <div id="single-project">
-            <a
-              href="http://ride-mern.herokuapp.com/#/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                alt=""
-                src="wheel-128.png"
-                onMouseEnter={rotateWheel}
-                onMouseLeave={mouseLeaveAnim}
-              />
-              <p>Ride</p>
-            </a>
-            <div>
-              <p>
-                Ride is a web application targeting the motorcycling enthusiast
-                community. It allows a user to record a ride on a map and peruse
-                all saved rides. Use the "post" a ride feature to share it or
-                stop by Ride and find your next adventure. Leave the community
-                your nugget of wisdom in a ride review. Our aim is to enhance
-                the enjoyment of two wheeled riding everywhere.
-                <ul>
-                  <li>Frontend: React / Redux, Mapbox</li>
-                  <li>Backend: Express.js</li>
-                  <li>DB: MongoDB</li>
                 </ul>
               </p>
             </div>
